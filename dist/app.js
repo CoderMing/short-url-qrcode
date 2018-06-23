58290d36995fe1a9ec6ddff20229f49d0398582a
coderming <coderming@foxmail.com>
2018-06-23 17:39:56 +0800

    更新ignore

--- dist/app.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const Koa = require("koa");
-const app = new Koa();
-app.use(async (ctx) => {
-    ctx.body = 'hello world';
-});
-app.listen(3000);
-console.log('Server running on port 3000');
