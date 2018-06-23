ada4cc9d52cdf0bb44a85888b5da0eade45ad735
coderming <coderming@foxmail.com>
2018-06-23 17:40:05 +0800

    更新ignore

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,9 @@
 import * as Koa from 'koa'
+import * as os from 'os'
 
 const app = new Koa()
+
+console.log(os.hostname())
 
 app.use(async (ctx: any) => {
   ctx.body = 'hello world'
@@ -8,4 +11,4 @@ app.use(async (ctx: any) => {
 
 app.listen(3000)
 
-console.log('Server running on port 3000');
+console.log('Server running on port 3000')
