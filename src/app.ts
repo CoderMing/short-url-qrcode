363797417ff2278ed698b97902e9a2038a70e6b1
coderming <coderming@foxmail.com>
2018-06-24 15:08:25 +0800

    引入router

--- src/app.ts
+++ src/app.ts
@@ -1,14 +1,27 @@
 import * as Koa from 'koa'
 import * as logger from 'koa-logger'
+import * as KoaRouter from 'koa-router'
+import * as bodyParser from 'koa-bodyparser'
+
+import conf from './config'
 
 const app = new Koa()
+const router = new KoaRouter()
 
 app.use(logger())
+app.use(bodyParser())
+app.use(router.routes())
+app.use(router.allowedMethods())
+
+router.get('/api', async ctx => {
+  ctx.body = 'api'
+})
 
 app.use(async ctx => {
   ctx.body = 'hello world'
 })
 
-app.listen(3000)
+app.listen(3000, () => {
+  console.log(`Server running on http://${conf.hostName}:${conf.port}`)
+})
 
-console.log('Server running on http://localhost:3000')
---
+++ src/config.ts
@@ -0,0 +1,11 @@
+interface Config {
+  port: number
+  hostName: string
+}
+
+const conf: Config = {
+  port: 3000,
+  hostName: 'localhost'
+}
+
+export default conf
