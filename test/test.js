05157f5c026e133bb3e7b5340432b908906a5689
coderming <coderming@foxmail.com>
2018-06-25 12:32:54 +0800

    假装有test

--- test/test.js
+++ test/test.js
@@ -1,1 +1,9 @@
-console.log('emmm')
+const expect = require('chai').expect
+
+const add = (a, b) => a + b 
+
+describe('假装是测试', function() {
+  it('1 加 1 应该等于 2', function() {
+    expect(add(1, 1)).to.be.equal(2);
+  });
+});
