701f0ae7ab5b4fbc1b3c52e3236c51dab05bcaa9
coderming <coderming@foxmail.com>
2018-06-27 10:07:00 +0800

    gulp再来一次压缩

--- gulpfile.js
+++ gulpfile.js
@@ -2,9 +2,10 @@ const gulp = require('gulp')
 const imageMin = require('gulp-imagemin')
 const stylus = require('gulp-stylus')
 const babel = require('gulp-babel')
+const uglify = require('gulp-uglify')
 
 gulp.task('image', () => {
-  gulp.src('src/images/**/*.*')
+  gulp.src('src/images/**/*.@(jpg|png|jpeg)')
       .pipe(imageMin({ progressive: true }))
       .pipe(gulp.dest('dist/images'))
 })
@@ -15,10 +16,11 @@ gulp.task('styles', () => {
       .pipe(gulp.dest('dist/styles'))
 })
 
-gulp.task('babel', () =>
+gulp.task('babel', () => {
   gulp.src('dist/**/*.js')
       .pipe(babel())
+      .pipe(uglify())
       .pipe(gulp.dest('dist/'))
-)
+})
 
 gulp.task('default', ['image', 'styles', 'babel'])
